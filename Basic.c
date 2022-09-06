7f15df1d14b0ce339e2c46d1cd8cf07cd57cdbef
samirvithlani <samir.vithlani83955@gmail.com>
2022-09-06 10:48:55 +0530

    basic

---
+++ Basic.c
@@ -0,0 +1,48 @@
+/*
+    objective:
+
+*/
+
+//standerd input output
+//# ->pre processive
+//include
+//top down
+#include<stdio.h>
+//main function
+//void is return type of function
+//; terminate symbol
+// variable -->variable is container to store specfic data..
+//data types
+//int float double long londouble char lld boolean
+
+/// 1 byte ? 8 bit
+///
+       //16-1
+///   2
+///  -32768 to 32767
+     //32-1
+/// 2
+/// variable declaration rules int a=10;,int a;
+/// variable name must not contain any space
+/// variable name must not starts with cap
+/// variable name must not starts with number
+/// variable name must not contain chae
+/// variable name must not conatains built in key word..
+///
+
+
+
+
+void main(){
+
+
+        int a =100,b = 200;
+        float f =12.25;
+        printf("value of a = %d",a);
+        printf("\n value of b = %d",b);
+
+        printf("\n float = %.2f",f);
+
+
+
+}
---
+++ scanf.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main(){
+
+
+/// address...
+    int no1,no2;
+    printf("\n address of no1 =%u",&no1);
+    printf("\n address of no2 =%u",&no2);
+    printf("\n enter no1 :");
+
+
+    scanf("%d",&no1);
+
+    printf("\n enter no2 :");
+    scanf("%d",&no2);
+
+
+    printf("\n value of no1 = %d",no1);
+    printf("\n value of no2 = %d",no2);
+
+}
